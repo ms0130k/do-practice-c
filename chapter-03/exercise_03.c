9b56bae62e5b2293ae026da2078d9a6f9410c6ba
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-11-04 22:11:02 +0900

    update201103

---
+++ chapter-03/exercise_03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void) {
+	printf("1) ===================");
+	char szText[32] = { 0 };
+	char ch1 = getchar();
+	char ch2 = getchar();
+	putchar(ch2);
+
+	printf("2) ===================");
+
+
+
+	return 0;
+}
---
+++ chapter-03/practice_03-01.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int nAge = 0;
+//	char szName[32] = { 0 };
+//
+//	printf("나이를 입력하세요.: ");
+//	scanf_s("%d%*c", &nAge);
+//
+//
+//	printf("이름을 입력하세요.: ");
+//	//fflush(stdin);
+//	gets_s(szName, sizeof(szName));
+//
+//	printf("이름: %s, 나이: %d", szName, nAge);
+//
+//	return 0;
+//}
