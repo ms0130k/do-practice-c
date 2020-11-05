a9d7427a5e68c7eddd0a1c3afee75b79b91a5bfa
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-11-05 09:09:27 +0900

    update 20201104

--- chapter-03/exercise_03.c
+++ chapter-03/exercise_03.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
 int main(void) {
-	printf("1) ===================");
+	/*printf("1) ===================");
 	char szText[32] = { 0 };
 	char ch1 = getchar();
 	char ch2 = getchar();
 	putchar(ch2);
 
-	printf("2) ===================");
+	printf("2) ===================");*/
+	char ch = getchar();
+	char szName[32] = { 0 };
+	gets(szName);
+	puts(szName);
+	printf("Å×½ºÆ® %s", &szName);
 
 
 
