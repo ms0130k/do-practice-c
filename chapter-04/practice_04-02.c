a196d6deb575360be1a2cef54a4bb8a0e8baed0b
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-11-20 07:36:31 +0900

    update practice_04-02

--- chapter-04/practice_04-02.c
+++ chapter-04/practice_04-02.c
@@ -4,7 +4,7 @@ int main(void) {
 	int nSecond = 0;
 
 	scanf_s("%d", &nSecond);
-	printf("%d초는 %d분 %02d초", nSecond, nSecond % 60);
+	printf("%d초는 %02d시간 %02d분 %02d초 입니다", nSecond, nSecond / 60 / 60, nSecond / 60 % 60, nSecond % 60);
 
 	return 0;
 }
