df2629e1a2bfb625f65903e9d11bc367bd8f604b
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-11-21 21:08:54 +0900

    update test

---
+++ chapter-04/opcast01.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int x = 5;
+//
+//	printf("%d\n", 5 / 2);
+//	printf("%d\n", (int)5.0 / 2);
+//	printf("%f\n", (double)5 / 2);
+//	printf("%f\n", 5.0 / 2);
+//	printf("%f\n", 5 / 2.0);
+//	printf("%f\n", (double)5 / 2);
+//	printf("%f\n", (double)x / 2);
+//	printf("%f\n", x / (double)2);
+//	printf("%f\n", (double)(x / 2));
+//
+//	return 0;
+//}
+//
+////나누기 전에 double 형이 되었는지 아니면 int 형을 나눈 후 double 형으로 변환한 것인지 구별할 수 있어야 합니다. 어떻게 구별함??
---
+++ chapter-04/simpleTest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(void) {
+	double a = 2.5;
+	int b = (int)a;
+
+	printf("test");
+}
