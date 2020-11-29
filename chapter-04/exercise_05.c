a706daf7f330338c2440ac176d719c7dd5782a9b
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-11-29 22:01:22 +0900

    update chapter04

---
+++ chapter-04/exercise_05.c
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int nResult, w = 0, x = -1, y = -1, z = 1;
+//	nResult = w++ || x++ && ++y || ++z;
+//	printf("%d %d %d %d %d\n", w, x, y, z, nResult);
+//}
+
+//if문은 다른 말로 분기문(branching statement)
---
+++ chapter-04/getmax.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int nMax = -100, nInput = 0;
+//
+//	scanf_s("%d", &nInput);
+//	nMax = nInput > nMax ? nInput : nMax;
+//
+//	scanf_s("%d", &nInput);
+//	nMax = nInput > nMax ? nInput : nMax;
+//
+//	scanf_s("%d", &nInput);
+//	nMax = nInput > nMax ? nInput : nMax;
+//
+//	printf("MAX: %d\n", nMax);
+//
+//	return 0;
+//}
+//
+////서바이벌 방식: 순차 검색(sequential search), 토너먼트 방식: 이분 검색(binary search)
