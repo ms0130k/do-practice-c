276253d703008d12bb46cb87639eef2dfc16bb5d
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-12-08 08:41:40 +0900

    update

--- chapter-07/triangle01.c
+++ chapter-07/triangle01.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
-
-int main(void)
-{
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int row = 10;
+//
+//	for (int i = 0; i < row; i++) {
+//		for (int j = 0; j < i + 1; j++) {
+//			putchar('*');
+//			putchar('\t');
+//		}
+//		putchar('\n');
+//	}
+//
+//	return 0;
+//}
