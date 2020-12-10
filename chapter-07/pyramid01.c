5c0929944d15fbfa817b5208328e29b6c9fdfeb1
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-12-10 12:09:05 +0900

    update

---
+++ chapter-07/pyramid01.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int i = 0, j = 0;
+//	for (i = 0; i < 5; i++) {
+//		for (j = 0; j < 5 + i; j++) {
+//			if (i + j < 4)	putchar('\t');
+//			else						printf("*\t");
+//		}
+//
+//		/*for (j = 0; j < 4 - i; j++) {
+//			putchar('\t');
+//		}
+//		for (j = 0; j < i * 2 + 1; j++) {
+//			printf("*\t");
+//		}*/
+//		putchar('\n');
+//	}
+//	return 0;
+//}
--- chapter-07/triangle02.c
+++ chapter-07/triangle02.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-
-int main(void)
-{
-	int length = 5;
-
-	/*for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < row - i - 1; j++)
-			putchar('\t');
-		for (int j = 0; j < i + 1; j++)
-			printf("*\t");
-		putchar('\n');
-	}*/
-
-	for (int i = 0; i < length; i++)
-	{
-		for (int j = 0; j < length; j++)
-		{
-			if (i + j < length - 1)
-				putchar('\t');
-			else
-				printf("*\t");
-		}
-		putchar('\n');
-	}
-
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int length = 5;
+//
+//	/*for (int i = 0; i < row; i++)
+//	{
+//		for (int j = 0; j < row - i - 1; j++)
+//			putchar('\t');
+//		for (int j = 0; j < i + 1; j++)
+//			printf("*\t");
+//		putchar('\n');
+//	}*/
+//
+//	for (int i = 0; i < length; i++)
+//	{
+//		for (int j = 0; j < length; j++)
+//		{
+//			if (i + j < length - 1)
+//				putchar('\t');
+//			else
+//				printf("*\t");
+//		}
+//		putchar('\n');
+//	}
+//
+//
+//	return 0;
+//}
