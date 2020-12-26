a2737656d4d33df0cfcfed30a3352bf1f787008a
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-12-27 00:17:42 +0900

    update

--- chapter-09/exercise_09-02.c
+++ chapter-09/exercise_09-02.c
@@ -3,14 +3,27 @@
 int main(void) {
 	int aList[5][5] = { 0 };
 	int i = 0, j = 0, nCounter = 0;
-	int x = 5, y = 0, nDirection = 1;
+	int x = 5, y = 0, nDirection = -1;
 
-	for (j = 0; j < 5; ++j)
+	for (i = 0; i < 5; ++i)
 	{
-		for (i = 0; i < 5; ++i)
+		for (j = 0; j < (5 - i) * 2 - 1; ++j)
 		{
-			if (j % 2 == 0) aList[i][j] = ++nCounter;
-			else aList[4 - i][j] = ++nCounter;
+			if (i % 2 == 0)
+			{
+				if (((5 - i) * 2 - 1) / 2 >= j) nDirection = -1;
+				else nDirection = 1;
+			}
+			else
+			{
+				if (((5 - i) * 2 - 1) / 2 >= j) nDirection = 1;
+				else nDirection = -1;
+			}
+
+			if (((5 - i) * 2 - 1) / 2 >= j) x += nDirection;
+			else y += nDirection;
+			aList[y][x] = ++nCounter;
+
 		}
 	}
 
