fc79dc6a6552c2b915c473c5a6165542672c9733
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2020-12-31 01:15:03 +0900

    pointer variable

---
+++ Chapter-11/test.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//int Test()
+//{
+//
+//}
+//
+//int main(void)
+//{
+//	int nData = 300;
+//	int* pnData = &nData;
+//	Test();
+//	pnData += 2;
+//	*pnData = 300;
+//	printf("*pnData: %d\n", *pnData);
+//	printf("nData: %d\n", nData);
+//	printf("pnData: %d\n", pnData);
+//
+//	return 0;
+//}
+//
+////&nData : 0x0019FED8
+////&pnData : 0x0019FECC
+////pnData : 0x0019FED8 > 0x0019FEDA
+////pnData : 0x0019FED8 > 0x0019FEE0
