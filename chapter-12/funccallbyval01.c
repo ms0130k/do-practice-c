8de84472388bd60e922d9fce1e2ae02fae390974
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2021-01-17 23:24:16 +0900

    update

--- chapter-12/funccallbyval01.c
+++ chapter-12/funccallbyval01.c
@@ -10,9 +10,15 @@ int Minus(int a, int b)
 	return a - b;
 }
 
+int Multiply(int a, int b)
+{
+	return a * b;
+}
+
 int main(int args, char* argv[])
 {
 	printf("%d\n", Add(3, 4));
 	printf("%d\n", Minus(7, 5));
+	printf("%d\n", Multiply(3, 21));
 	return 0;
 }
